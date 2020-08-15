f79eb7703bc611894ffa31048710a0ebe61d62b1
Belchior <Always.X26@gmail.com>
2020-08-15 14:03:54 -0300

    Esqueci de subir a classe CursoController
    Eu havia esquecido de subir a classe, me deparei com isso hoje quando fui olhar meu git, e subi agora..

---
+++ src/main/java/com/rd/treinamentodev/AvaliacaoSpringBoot/controller/CursoController.java
@@ -0,0 +1,24 @@
+package com.rd.treinamentodev.AvaliacaoSpringBoot.controller;
+
+import com.rd.treinamentodev.AvaliacaoSpringBoot.model.dto.CursoDTO;
+import com.rd.treinamentodev.AvaliacaoSpringBoot.service.CursoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CursoController {
+    @Autowired
+    private CursoService cursoService;
+
+    @PostMapping("/cadastro-curso")
+    public ResponseEntity registrarCurso(@RequestBody CursoDTO dto) {
+        cursoService.inserir(dto);
+        return ResponseEntity.ok().body(dto);
+    }
+}
+
+
+
