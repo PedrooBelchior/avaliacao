e9782329317de6653d5d31db068ea120b56d213f
Belchior <Always.X26@gmail.com>
2020-08-15 14:06:32 -0300

    Esqueci de colocar a notação @Repository

--- src/main/java/com/rd/treinamentodev/AvaliacaoSpringBoot/repository/CursoRepository.java
+++ src/main/java/com/rd/treinamentodev/AvaliacaoSpringBoot/repository/CursoRepository.java
@@ -1,9 +1,11 @@
 package com.rd.treinamentodev.AvaliacaoSpringBoot.repository;
 import com.rd.treinamentodev.AvaliacaoSpringBoot.model.entity.CursoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import java.util.List;
 
+@Repository
 public interface CursoRepository extends JpaRepository<CursoEntity, Long> {
     //Questão 4--> Implementação de método de busca por nome do curso que retorna uma lista de cursos:
-    List<CursoEntity> buscarNomeCurso(String nomeCurso); // select * from tb_curso where nome = ?
+    List<CursoEntity> findByNomeCurso (String nomeCurso); // select * from tb_curso where nome = ?
 }
